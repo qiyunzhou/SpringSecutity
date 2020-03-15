4446971c94cc2170bac2a566505f44790b47fa83
漆云州 <15086314018@163.com>
2020-03-15 14:21:53 +0800

    spring security环境搭建

---
+++ security-spring-security/src/main/java/pers/qyz/security/springmvc/config/WebConfig.java
@@ -0,0 +1,34 @@
+package pers.qyz.security.springmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pers.qyz.security.springmvc"
+        ,includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,value = Controller.class)})
+public class WebConfig implements WebMvcConfigurer {
+
+    // 视频解析器
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+    }
+
+    // spring security不需要重写拦截器
+}
---
+++ security-spring-security/src/main/java/pers/qyz/security/springmvc/init/SpringApplicationInitializer.java
@@ -0,0 +1,23 @@
+package pers.qyz.security.springmvc.init;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import pers.qyz.security.springmvc.config.ApplicationConfig;
+import pers.qyz.security.springmvc.config.WebConfig;
+
+public class SpringApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{ApplicationConfig.class};    //指定rootContext的配置类
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {    //指定servletContext的配置类
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
